a8b5f5cd6605f384022b3c071ff2c2f82b7ff449
Brock Nelson <Bmnelson87@gmail.com>
2023-10-18 15:43:54 -0500

    created SQL server, added router.get, added useEffect and axios to App.js, retrieved stored data from server and displayed on DOM via JSON.stringify

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,6 +3,18 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
+router.get('/', (req,res) => {
+    const sqlText = `SELECT * FROM "tasks" ORDER BY "priority" DESC;`
+    pool.query(sqlText)
+    .then((result) => {
+        console.log('Data retrieved from tasks SQL table', result);
+        res.send(result.rows);
+    })
+    .catch ((error)=> {
+        console.log(`Error in making database query ${sqlText}`, error);
+        res.sendStatus(500);
+    })
+})
 
 // POST
 
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,28 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App () {
   
+  const [taskList, setTaskList] = useState([]);
+
+  const getTaskList = () => {
+    axios.get('/todo').then((response) => {
+      console.log(response.data);
+      setTaskList(response.data);
+    }).catch ((error) => {
+      console.log('Error in getTaskList', error);
+      alert("Tasks could not be brought from server");
+    })
+  }
+
+  useEffect(() =>{
+    getTaskList();
+  }, []);
+
   return (
     <div>
       <h1>TO DO APP</h1>
+      {JSON.stringify(taskList)}
     </div>
   );
 
